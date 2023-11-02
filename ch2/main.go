d390cd8358b9472568e7d64850bb403d4ac97c34
Umair Khan <m.umairkhan.co@proton.me>
2023-11-02 23:13:06 +0500

    updated Chapter 2

---
+++ ch2/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// Constant
+const x int64 = 10
+
+func main(){
+	// Booleans 
+	var flag bool
+	var isAwersome = true
+	
+	fmt.Println(flag,isAwersome)	
+
+	// Int
+	var x int = 10
+	x *= 2
+	fmt.Println(x)
+	
+	// Complex Types
+	com := complex(1,2)
+	fmt.Println(real(com))
+	fmt.Println(imag(com))
+
+}
+
